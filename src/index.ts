5a566d5065ce2ab3e2607c2d198e399c8461c39c
yiminwook <grs0416@gmail.com>
2023-12-18 18:24:24 +0900

    Add: card box 추가

--- src/index.ts
+++ src/index.ts
@@ -51,6 +51,31 @@ const wordflick = () => {
   }, speed);
 };
 
+const cardMouseEvent = () => {
+  $(".card").on("mousemove", (e) => {
+    const x = e.offsetX;
+    const y = e.offsetY;
+    const rotateX = (4 / 30) * y - 20;
+    const rotateY = (-1 / 5) * x + 20;
+
+    //  마우스 위치      카드회전율   꼭지점위치
+    // (   0,   0 )  => ( -20,  20 )  1사분면
+    // ( 220,   0 )  => ( -20, -20 )  2사분면
+    // (   0, 310 )  => (  20,  20 )  3사분면
+    // ( 220, 310 )  => (  20, -20 )  4사분면
+
+    $(e.currentTarget).css(
+      "transform",
+      `perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+    );
+  });
+
+  $(".card").on("mouseleave", (e) => {
+    $(e.currentTarget).css("transform", "");
+  });
+};
+
 $(() => {
   wordflick();
+  cardMouseEvent();
 });
