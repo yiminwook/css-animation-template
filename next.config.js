971ec0a6485d31cf0905d7f6ea29f551fa6e0ff8
yiminwook <grs0416@gmail.com>
2023-12-19 10:06:07 +0900

    Update: build option

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const REPOSITORY = "css-animation-template";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // output: "export",
   reactStrictMode: true,
   assetPrefix: !isDevMode ? `/${REPOSITORY}/` : "", // production 일때 prefix 경로
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
